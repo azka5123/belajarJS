aacd5ef0d3f4ecb6bfa6e5e26934eb8a36818373
azka.thoyyib@gmail.com <azka.thoyyib@gmail.com>
2022-11-17 10:03:59 +0700

    js

---
+++ js/gantiFont.js
@@ -0,0 +1,9 @@
+function gantiFont(){
+    document.getElementById("test").style.fontSize="50px";
+}
+
+function bye(){
+    document.getElementById("test").innerHTML="bye";
+    document.getElementsByTagName("test").i
+}
+
